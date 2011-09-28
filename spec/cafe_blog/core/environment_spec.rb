69888906a967fefa8f8d60ad0f6e01a0d3495ecc
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-09-28 20:25:56 +0900

    refactoring: environment_spec.rb

--- spec/cafe_blog/core/environment_spec.rb
+++ spec/cafe_blog/core/environment_spec.rb
@@ -1,79 +1,72 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe 'CafeBlog::Core::Environment' do
+  def _setup_(*args); CafeBlog::Core::Environment.setup(*args) end
+  def _valid_setup_(params = {}); _setup_( {:database => @database}.merge(params) ) end
+
+  before :all do
+    @database = Sequel.connect('sqlite:///')
+  end
   before do
     CafeBlog::Core::Environment.instance_variable_set(:@instance, nil)
   end
-  def setup_parameter(params = {})
-    {:database => Sequel.connect('sqlite:/')}.merge(params)
+
+  shared_context 'after .setup' do
+    let(:parameter) { {} }
+    before { @ins = CafeBlog::Core::Environment.setup( {:database => @database}.merge(parameter) ) }
   end
 
-  it 'is class' do
-    CafeBlog::Core::Environment.should be_instance_of(Class)
-  end
-
-  it '.new is not public method' do
-    lambda { CafeBlog::Core::Environment.new }.should raise_error(NoMethodError)
-  end
+  subject { CafeBlog::Core::Environment }
+  it { should be_instance_of(Class) }
+  it { should_not respond_to(:new) }
 
   describe '.setup' do
-    it 'raise error received except for hash parameter' do
-      lambda { CafeBlog::Core::Environment.setup(:nil) }.should raise_error(ArgumentError)
+    context 'received except for hash parameter' do
+      it { expect { _setup_(:nil) }.to raise_error(ArgumentError) }
     end
-    it 'raise error received no parameter' do
-      lambda { CafeBlog::Core::Environment.setup() }.should raise_error(ArgumentError)
+    context 'received no parameter' do
+      it { expect { _setup_ }.to raise_error(ArgumentError) }
     end
-    it 'raise error received except for sequel database' do
-      lambda { CafeBlog::Core::Environment.setup(:database => 'bad parameter') }.should raise_error(ArgumentError)
+    context 'received except for sequel database' do
+      it { expect { _setup_(:database => 'bad parameter') }.to raise_error(ArgumentError) }
     end
-    it 'raise error received no :database parameter' do
-      lambda { CafeBlog::Core::Environment.setup(:sample => 123456789) }.should raise_error(ArgumentError)
+    context 'received no :database parameter' do
+      it { expect { _setup_(:sample => 123456789) }.to raise_error(ArgumentError) }
     end
     
-    it 'return Environment instance' do
-      CafeBlog::Core::Environment.setup(setup_parameter).should be_instance_of(CafeBlog::Core::Environment)
+    context 'result value' do
+      include_context('after .setup')
+      subject { @ins }
+      it { should be_instance_of(CafeBlog::Core::Environment) }
     end
   end
 
   describe '.instance' do
-    it 'is nil before setup' do
-      CafeBlog::Core::Environment.instance.should be_nil
+    subject { CafeBlog::Core::Environment.instance }
+    context 'before .setup' do
+      it { should be_nil }
     end
-    it 'is set instance after setup' do
-      ins = CafeBlog::Core::Environment.setup(setup_parameter)
-      CafeBlog::Core::Environment.instance.should == ins
-    end
-
-    it 'raise already created instance' do
-      CafeBlog::Core::Environment.setup(setup_parameter)
-      lambda { CafeBlog::Core::Environment.setup(setup_parameter) }.should raise_error(CafeBlog::Core::ApplicationError)
+    context 'after .setup' do
+      include_context('after .setup')
+      it { should == @ins }
+      it { expect { _valid_setup_ }.to raise_error(CafeBlog::Core::ApplicationError) }
     end
   end
   
   describe '.check_instance' do
-    before :each do
-      CafeBlog::Core::Environment.instance_eval { @instance = nil }
+    subject { CafeBlog::Core::Environment.check_instance }
+    context 'before .setup' do
+      it { expect { CafeBlog::Core::Environment.check_instance }.to raise_error(CafeBlog::Core::ApplicationError) }
     end
-    it 'raise ApplicationError before setup' do
-      lambda { CafeBlog::Core::Environment.check_instance }.should raise_error(CafeBlog::Core::ApplicationError)
-    end
-    it 'is return instance after setup' do
-      ins = CafeBlog::Core::Environment.setup(setup_parameter)
-      CafeBlog::Core::Environment.check_instance.should == ins
+    context 'after .setup' do
+      include_context('after .setup')
+      it { should == @ins }
     end
   end
 
   describe '#database' do
-    before :all do
-      @database = Sequel.connect('sqlite:///')
-    end
-    before :each do
-      CafeBlog::Core::Environment.instance_eval { @instance = nil }
-      @env = CafeBlog::Core::Environment.setup(:database => @database)
-    end
-
-    it 'is instance of Sequel::Database' do
-      @env.database.should be_kind_of(Sequel::Database)
-    end
+    include_context('after .setup')
+    subject { CafeBlog::Core::Environment.instance.database }
+    it { should be_kind_of(Sequel::Database) }
   end  
 end
