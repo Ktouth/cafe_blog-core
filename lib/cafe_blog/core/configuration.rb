c38c9195906ce7f830e0834baa36bf06d5e3f126
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-11-07 18:19:42 +0900

    add: CafeBlog::Core::Configuration.key update: CafeBlog::Core.Configuration

--- lib/cafe_blog/core/configuration.rb
+++ lib/cafe_blog/core/configuration.rb
@@ -1,8 +1,48 @@
+require 'singleton'
+
 module CafeBlog
   module Core
     # 環境情報保持クラス
     class Configuration
-      
+      class <<self
+        # データベースに保持する際に使用する識別キー
+        attr_reader :key
+      end
+
+      private
+
+      def initialize
+        @values = self.class.instance_variable_get(:@initialize_values).inject({}) do |r, (k, v)|
+          case v
+          when Proc
+            r[k] = v.call
+          else
+            r[k] = v
+          end
+          r
+        end
+      end      
+    end
+
+    # 環境変数保持クラスのサブクラスを生成する
+    # @param [String] key 保持のための使用される識別キー
+    # @param [Hash] values データベースに保持される属性メソッドとその初期値のハッシュ
+    # @return [Class] {Configuration}のサブクラス
+    # @raise [ArgumentError] +key+が適切な文字列ではない
+    # @raise [ArgumentError] +values+が適切なハッシュではない
+    def self.Configuration(key, values)
+      raise ArgumentError, "#{key.inspect}は適切な文字列ではありません" unless key.is_a?(String) && key =~ /^(?!\d)[a-z\d_]+$/
+      raise ArgumentError, "#{values.class}は適切なハッシュではありません" unless values.is_a?(Hash)
+
+      Class.new(Configuration).tap do |c|
+        c.instance_variable_set(:@key, key)
+        c.instance_variable_set(:@initialize_values, values)
+        values.each do |sym, val|
+          c.module_eval("def #{sym}; @values[#{sym.inspect}] end")
+          c.module_eval("def #{sym}=(value); @values[#{sym.inspect}] = value end")
+        end
+        c.module_eval { include Singleton }
+      end
     end
   end
 end
--- spec/cafe_blog/core/configuration_spec.rb
+++ spec/cafe_blog/core/configuration_spec.rb
@@ -4,3 +4,55 @@ describe 'CafeBlog::Core::Configuration' do
   subject { CafeBlog::Core::Configuration }
   it { should be_a(Class) }
 end
+
+describe 'CafeBlog::Core.Configuration' do
+  before :all do
+    @valid_key = 'sample'
+    @valid_values = {:foo => true, :bar => 'test', :baz => proc { Hash.new } }
+  end
+  subject { CafeBlog::Core }
+  it { should respond_to(:Configuration) }
+
+  it { expect { subject.Configuration }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key, @valid_values, 1235651) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key, @valid_values) }.to_not raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(156489123, @valid_values) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(/regexp/, @valid_values) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(:symbol, @valid_values) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(Class, @valid_values) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration('Class', @valid_values) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration('112345691235', @valid_values) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key, /1123156/) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key, -1535.51) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key, Time.now) }.to raise_error(ArgumentError) }  
+  it { expect { subject.Configuration(@valid_key, Module) }.to raise_error(ArgumentError) }  
+
+  describe 'return value' do
+    subject { CafeBlog::Core::Configuration(@valid_key, @valid_values) }
+    it { should be_a(Class) }
+    it { should < CafeBlog::Core::Configuration }
+    it { should respond_to(:key) }
+    it { subject.key.should == @valid_key }
+  end
+
+  describe 'singleton class' do
+    before :all do
+      @class = CafeBlog::Core::Configuration(@valid_key, @valid_values)
+    end
+    subject { @class }
+    it { should_not respond_to(:new) }
+    it { should respond_to(:instance) }
+    it { @class.instance.should == @class.instance }
+  end
+
+  describe 'getter and setter methods' do
+    before :all do
+      @class = CafeBlog::Core::Configuration(@valid_key, @valid_values)
+    end
+    subject { @class.instance }
+    it { @valid_values.keys.all? {|x| subject.respond_to? x }.should be_true }
+    it { @valid_values.keys.all? {|x| subject.respond_to?("#{x}=") }.should be_true }
+    it { @valid_values.all? {|k, v| subject.send(k) == (v.is_a?(Proc) ? v.call : v) }.should be_true }
+  end
+end
