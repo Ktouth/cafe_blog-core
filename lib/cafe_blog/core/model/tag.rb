5d51b2c751717d889e49b325ce5b535fe500bf2b
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-12-02 14:51:20 +0900

    update(document): CafeBlog::Core::Model::Tag#code / #to_code

--- lib/cafe_blog/core/model/tag.rb
+++ lib/cafe_blog/core/model/tag.rb
@@ -6,7 +6,7 @@ module CafeBlog
       # タグ情報に対応するモデルクラス
       # @attr [Integer] id タグを識別する番号。新規作成時に自動設定される
       # @attr [String] name タグ名。一意的なもののみを受け付ける
-      # @attr [String] code タグを識別するコード名。省略可。省略時には 'tag%04d' % #id が適用される。 #to_code を参照。タグによる絞り込みやURLなどで使用
+      # @attr [String] code タグを識別するコード名。省略可。 #to_code を参照。タグによる絞り込みやURLなどで使用
       #   タグコードは3字以上16字以内で先頭が英子文字で始まる英小文字と数字およびアンダーバーのみで構成されているもののみを受け付ける
       class Tag < Core::Model(:tags)
         restrict_primary_key
@@ -16,7 +16,7 @@ module CafeBlog
         validates(:code, :allow_nil => true) { uniqueness and length(:minimum => 3, :maximum => 16) and format(:with => /^(?![_\d])[a-z\d_]+$/) }
 
         # タグに対応するコードを取得する
-        # #code が設定されている時はそれが、設定されていない既存タグは 'tag%04d' % #id が、#code が設定されてない新規タグは 'tag0000' を返す
+        # @note #code が設定されている時はそれが、設定されていない既存タグは 'tag%04d' % #id が、#code が設定されてない新規タグは 'tag0000' を返す
         # @return [String] タグコード
         def to_code
           code || ('tag%04d' % (id || 0))
